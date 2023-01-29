01aef4e6f3b840289019e95eb3fc063fd3daadec
Marek Purwin <purwinmarek16@gmail.com>
2023-01-29 13:56:06 +0100

    add spinner

--- projects/luxui/src/lib/ui/spinner/spinner.component.ts
+++ projects/luxui/src/lib/ui/spinner/spinner.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lx-spinner',
   templateUrl: './spinner.component.html',
   styleUrls: ['./spinner.component.scss'],
 })
-export class SpinnerComponent implements OnInit {
-  constructor() {}
+export class SpinnerComponent {
+  @Input() type: 'classic' = 'classic';
+  @Input() spinnerColor: string = '#333';
+  @Input() textColor: string = '#333';
+  @Input() text!: string;
 
-  ngOnInit(): void {}
+  constructor() {}
 }
--- src/app/documentation/ui/pages/loading-spinner-page/loading-spinner-page.component.ts
+++ src/app/documentation/ui/pages/loading-spinner-page/loading-spinner-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner-page',
   templateUrl: './loading-spinner-page.component.html',
-  styleUrls: ['./loading-spinner-page.component.scss']
+  styleUrls: ['./loading-spinner-page.component.scss'],
 })
-export class LoadingSpinnerPageComponent implements OnInit {
+export class LoadingSpinnerPageComponent {
+  public isLoading = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  public props = [
+    {
+      name: '[type]',
+      desc: 'type of spinner',
+      type: "'classic'",
+      default: "'classic'",
+    },
+    {
+      name: '[spinnerColor]',
+      desc: 'color of the spinner',
+      type: 'string',
+      default: '#333',
+    },
+    {
+      name: '[text]',
+      desc: 'whether the text is inside the spinner',
+      type: 'string',
+      default: '-',
+    },
+    {
+      name: '[textColor]',
+      desc: 'color of the text',
+      type: 'string',
+      default: '#333',
+    },
+  ];
+  constructor() {}
 }
